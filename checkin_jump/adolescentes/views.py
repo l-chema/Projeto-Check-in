e338c5953dcc197fc2521cc250b991ffa45972cd
Neivinha <lucascpereira39@gmail.com>
2025-04-06 15:49:51 -0300

    Revisao0604

--- checkin_jump/adolescentes/views.py
+++ checkin_jump/adolescentes/views.py
@@ -1,8 +1,10 @@
+from datetime import datetime
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Adolescente
 from .forms import AdolescenteForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.messages import get_messages
 from django.contrib.auth.decorators import login_required
 
 def login_view(request):
@@ -33,18 +35,30 @@ def listar_adolescentes(request):
 def criar_adolescente(request):
     if request.method == "POST":
         data_nascimento = request.POST.get("data_nascimento")
-        if data_nascimento and datetime.strptime(data_nascimento, '%Y-%m-%d') > datetime.now():
-            messages.error(request, "A data de nascimento não pode ser no futuro.")
-            return redirect('criar_adolescente')
 
-    if request.method == "POST":
+        # Limpa mensagens antigas
+        storage = get_messages(request)
+        list(storage)  # Isso esvazia o iterador de mensagens
+
+        # Validação de data
+        if data_nascimento:
+            try:
+                data = datetime.strptime(data_nascimento, '%d/%m/%Y')
+                if data > datetime.now():
+                    messages.error(request, "A data de nascimento não pode ser no futuro.")
+                    return redirect('criar_adolescente')
+            except ValueError:
+                messages.error(request, "Formato de data inválido. Use dd/mm/aaaa.")
+                return redirect('criar_adolescente')
+
+        # Processa formulário
         form = AdolescenteForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('listar_adolescentes')
     else:
         form = AdolescenteForm()
-    return render(request, 'adolescentes/form.html', {'form': form})
+    return render(request, 'adolescentes/criar_adolescente.html', {'form': form})
 
 @login_required
 def editar_adolescente(request, id):
