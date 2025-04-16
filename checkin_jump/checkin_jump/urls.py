cf5e8d3bc0040b2b6e8289aabf576b2ae39d8d4b
duduetc <eduardo.etca@gmail.com>
2025-04-16 10:08:12 -0300

    não sei o que estou fazendo da minha vida

--- checkin_jump/checkin_jump/urls.py
+++ checkin_jump/checkin_jump/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('adolescentes/', include('adolescentes.urls')),
+    path('adolescentes/', include('checkin_jump.adolescentes.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   
