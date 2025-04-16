8a162f0f0327d71f55b15581ea28e10417ba44f5
duduetc <eduardo.etca@gmail.com>
2025-04-16 10:18:39 -0300

    DEUS CONTINUE NOS ABENÇOANDO

--- adolescentes/apps.py
+++ adolescentes/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AdolescentesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'checkin_jump.adolescentes'
+    name = 'adolescentes'
