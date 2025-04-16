8ea68e2b59cfff18c30683f4f1660c2f704edc83
duduetc <eduardo.etca@gmail.com>
2025-04-16 14:27:38 -0300

    sera q vai

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'checkin_jump.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Projeto-Check-in-main.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
